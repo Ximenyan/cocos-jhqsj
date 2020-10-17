d0ac74040f6aef3812b0571d54498cb2d0571db7
ximenyan <347364217@qq.com>
2020-10-17 13:31:56 +0800

    第一版合约完成

--- contract/contract.jh-farms.lua
+++ contract/contract.jh-farms.lua
@@ -35,6 +35,7 @@ end
 
 local function _get_nft_contract_info(nft_id) 
     local nft = cjson.decode(chainhelper:get_nft_asset(nft_id))
+    assert(nft.world_view == G_CONFIG.FARMS_WORLD_VIEW, "#世界观不正确！#")
     for _, contract in pairs(nft.describe_with_contract) do
         if contract[1] == contract_base_info.id then
             for _, describe in pairs(contract[2]) do
@@ -64,6 +65,7 @@ function create()
     for i=0,9 do 
         nft_contract_info["land_" .. i] = {}
     end
+    nft_contract_info.owner = contract_base_info.caller
     -- 记录农田信息
     private_data.farm = { 
         nft_id = nft_id
@@ -166,6 +168,7 @@ function BuyLand(args)
     local land_key = _args[1]
     local shovel_id = _args[2]
     -- 检查铁锹ID是否正确
+    assert(string.sub(land_key,1,4) == "land","#参数land_id不正确#")
     assert(string.sub(shovel_id, 1, 9) == PRE_SHOVEL_GID,
             "#参数shovle_id不正确#")
     local farm = private_data.farm
@@ -203,7 +206,7 @@ function Plant(args)
     assert(string.sub(seed_id, 1, 7) == PRE_SEED_GID, "#别乱种！#")
     local nft_land = _get_nft_contract_info(nft_id)
     local land = nft_land[land_key]
-    assert(land ~= nil,"#土地还未开垦！#")
+    assert(land.status ~= nil,"#土地还未开垦！#")
     assert(land.status == false, "#这块土地已经种上了!#")
     -- 消耗一颗种子
     CPlayerPackage.spent_item(seed_id, 1)
@@ -248,9 +251,15 @@ function Reap(args)
     end
     -- 保存数据
     local efficiency = OUTPUT_EFFICIENCY[land.star]
-    local total = plant.uint * time_long * efficiency * (1 - 0.1 * land.steal_count)/ 10
+    local steal_success_count = 0
+    if  land.steal_info then 
+        for _, value in pairs(land.steal_info) do
+            if value[0] then steal_success_count = steal_success_count + 1 end
+        end
+    end
+    local total = plant.uint * time_long * efficiency * (1 - 0.1 * steal_success_count)/ 10
     total = math.floor(total)
-    chainhelper:log('收获' .. total .. token_id)
+    chainhelper:log('收获' .. (total/100000) .. token_id)
     -- 解锁并转发到账户
     CToken.TransferOut(token_id, total)
     -- 记录到nft以备共享数据
@@ -269,12 +278,14 @@ function Steal(args)
     local _args = cjson.decode(args)
     assert(#_args == 2, "#参数不对！#")
     local farm = private_data.farm
+    local attrs = private_data.attrs
+    assert(farm.sacrifice,"#还没献祭呢！#")
     local nft_id = _args[1]
     local land_key = _args[2]
     assert(nft_id ~= farm.nft_id, "#不能偷自己的！#")
     local nft_land = _get_nft_contract_info(nft_id)
     local land =  nft_land[land_key]
-    assert(land ~= nil,"#土地未开垦！#")
+    assert(land.status ~= nil,"#土地未开垦！#")
     assert(land.status, "#土地未种植！#")
     if land.steal_count == nil then land.steal_count = 0 end
     if land.steal_info == nil then land.steal_info = {} end
@@ -291,20 +302,62 @@ function Steal(args)
     end
     -- 偷取
     local efficiency = OUTPUT_EFFICIENCY[land.star]
-    local total = plant.uint * time_long * efficiency * (1 - 0.1 * land.steal_count)/ 10
-    total = math.floor(total * 0.1)
-    chainhelper:log(contract_base_info.caller .. '偷取' .. total .. token_id)
-    -- 解锁并转发到账户
-    CToken.TransferOut(token_id, total)
-    -- 罚金
-    CToken.TransferIn("COCOS", G_CONFIG.STEAL_FORFEIT)
+    -- 计算成功还是失败
+    -- 星级越高，防御力越高，哈哈哈，完美
+    -- 失败概率
+    -- 1 星 50%
+    -- 2 星 52%
+    -- 3 星 54%
+    -- 4 星 56%
+    -- 5 星 58%
+    -- 6 星 60%
+    local random_number = chainhelper:random() % 10000
+    local total = 0
+    local status = true
+    -- 现质押罚金
+    local forfeit = G_CONFIG.STEAL_FORFEIT * land.star
+    chainhelper:transfer_from_caller(contract_base_info.owner, forfeit,"COCOS",true)
+    if random_number > ((land.star-1) * 200 + 5000 - attrs.innate_attr.luck) then 
+        local steal_success_count = 0
+        for _, value in pairs(land.steal_info) do
+            if value[0] then steal_success_count = steal_success_count + 1 end
+        end
+        total = plant.uint * time_long * efficiency * (1 - 0.1 * steal_success_count) / 10
+        total = math.floor(total * 0.1)
+        chainhelper:log(contract_base_info.caller .. '偷取' .. (total/100000) .. token_id)
+        -- 解锁并转发到账户
+        CToken.TransferOut(token_id, total)
+        -- 返还罚金
+        chainhelper:transfer_from_owner(contract_base_info.caller, forfeit, "COCOS", true)
+    else 
+        status = false
+        -- 把罚金95%送给农田主人
+        local player_forfeit = math.floor(forfeit * 0.95)
+        -- 返还罚金
+        chainhelper:transfer_from_owner(nft_land.owner, player_forfeit, "COCOS", true) 
+        -- log
+        chainhelper:log(contract_base_info.caller .. '偷窃被抓,罚款' .. (forfeit/100000) .. "COCOS")
+        total = player_forfeit
+    end
     -- 记录到nft以备共享数据
     land.steal_count = land.steal_count + 1 -- 被偷次数+1
-    land.steal_info[contract_base_info.caller] = total -- 记录偷盗者信息，方便报仇，雪恨
+    land.steal_info[contract_base_info.caller] = {status, total} -- 记录偷盗者信息，方便报仇，雪恨
     nft_land[land_key] = land
     chainhelper:change_nht_active_by_owner(contract_base_info.caller, nft_id, false)
     chainhelper:nht_describe_change(nft_id,NFT_CONTRACT_INFO,cjson.encode(nft_land),false)
     chainhelper:change_nht_active_by_owner(contract_base_info.owner, nft_id, false)
+end 
+
+function Sacrifice(args) 
+    local _args = cjson.decode(args)
+    assert(#_args == 1, "#参数不对！#")
+    local farm = private_data.farm
+    local COIN_SYMBOL = _args[1]
+    local amount = G_CONFIG.SACRIFICE_COINS[COIN_SYMBOL]
+    assert(amount ~= nil,"#不能用这个货币献祭！#")
+    CToken.TransferIn(COIN_SYMBOL,amount)
+    chainhelper:log("献祭成功！")
+    farm.sacrifice = true
 end
 
 function test() chainhelper:log('!- 3') end
