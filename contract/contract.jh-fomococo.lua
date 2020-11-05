a59d45dc2503a44997e72d6797597a1819a97986
wangwei <347364217@qq.com>
2020-11-05 19:53:27 +0800

    add fomo

--- contract/contract.jh-fomococo.lua
+++ contract/contract.jh-fomococo.lua
@@ -1,6 +1,7 @@
-DEVELOPER_PROPORTION = 0.05 --开发者占比
-BOUNS_PROPORTION = 0.75 --分红占比
-FINAL_PRIZE = 0.2
+DEVELOPER_PROPORTION = 0.02 --开发者占比
+BOUNS_PROPORTION = 0.48 --分红占比
+FINAL_PRIZE = 0.35 --最终大奖
+WIN_COUNTRY = 0.15 --获胜国家分红
 
 local function _read_data()
     -- 读数据
@@ -19,12 +20,16 @@ function init()
     _read_data()
     --assert(public_data.count == nil, "#游戏已经开始了#");
     public_data = {
-        count = 0,
-        timestamp = chainhelper:time(),
+        count = 0, -- 轮数
+        total_amout = 0,--总流水
+        count_amount = 0,--本轮总流水
+        win_country_amount = 0,--获胜国家分红
+        final_prize = 0,--最终大奖
+        timestamp = chainhelper:time() + 24 * 60 * 60,
         countrys = {
-            wei = {num = 0, price = 0},
-            shu = {num = 0, price = 0},
-            wu = {num = 0, price = 0}
+            Wei = {num = 0, price = 0},
+            Shu = {num = 0, price = 0},
+            Wu = {num = 0, price = 0}
         }
     }
     _save_data()
@@ -33,11 +38,28 @@ end
 function Buy(arg_country, arg_num)
     _read_data()
     arg_num = math.floor(arg_num)
+    if public_data.winner ~= nil and chainhelper:time() > public_data.timestamp then
+        -- 这一轮战争已经结束了，作为开奖人，将获得最终大奖的1%，然后开启下一轮战争
+        -- 最终大奖的99% 将归终结者所有
+        local winner_amount = math.floor(public_data.final_prize * 0.99) --这是发给获胜人的
+        chainhelper:log(public_data.winner .. '  Won the war！amount :' .. (winner_amount/100000))
+        local call_amount = public_data.final_prize - winner_amount -- 这是发给开奖人的
+        chainhelper:log(contract_base_info.caller .. ' Draw a prize! amount :' .. (call_amount/100000))
+        local win_country = public_data.countrys[public_data.win_country]
+        chainhelper:log(public_data.win_country .. ' Carve Up Amount :' .. (public_data.win_country_amount/100000))
+        win_country.price = win_country.price + math.floor(public_data.win_country_amount/win_country.num) --分红给战胜国
+        public_data.count = public_data.count + 1
+        public_data.count_amount = 0
+        public_data.win_country_amount = 0
+        public_data.final_prize = 0
+        public_data.timestamp = chainhelper:time() + 24 * 60 * 60 --战争时间新增一天
+        chainhelper:log(public_data.win_country .. " Won the war!A new round of war has begun!")
+    end
     if private_data.countrys == nil then 
         private_data.countrys ={
-            wei = {num = 0, price = 0},
-            shu = {num = 0, price = 0},
-            wu = {num = 0, price = 0}
+            Wei = {num = 0, price = 0},
+            Shu = {num = 0, price = 0},
+            Wu = {num = 0, price = 0}
         }
     end
     local public_country = public_data.countrys[arg_country]
@@ -46,6 +68,7 @@ function Buy(arg_country, arg_num)
     local bonus_amount = math.floor(amount * BOUNS_PROPORTION / 3)
     local final_prize = math.floor(amount * FINAL_PRIZE)
     local dev_amount = math.floor(amount * DEVELOPER_PROPORTION)
+    local win_country_amount = math.floor(amount * WIN_COUNTRY)
     for name, country in pairs(public_data.countrys) do
         if country.num ~= 0 then 
             public_data.countrys[name].price = country.price + math.floor(bonus_amount/country.num)
@@ -62,6 +85,12 @@ function Buy(arg_country, arg_num)
     end
     private_country.num = private_country.num + arg_num
     public_country.num = public_country.num + arg_num
+    public_data.total_amout = public_data.total_amout + math.floor(amount/100000) -- 总流水
+    public_data.final_prize = public_data.final_prize + final_prize --最终大奖
+    public_data.timestamp = public_data.timestamp + arg_num * 10 -- 战争时间延长
+    public_data.win_country = arg_country -- 记录领先国家
+    public_data.winner = contract_base_info.caller -- 记录领先玩家
+    public_data.win_country_amount = public_data.win_country_amount + win_country_amount
     _save_data()
 end
 
@@ -69,6 +98,12 @@ function Withdraw()
     -- public_data = {}
     private_data = {}
     write_list = {private_data = {}}
-    chainhelper:log(chainhelper:number_max())
+    chainhelper:write_chain()
+end
+
+function clear()
+    -- public_data = {}
+    private_data = {}
+    write_list = {private_data = {}}
     chainhelper:write_chain()
 end
