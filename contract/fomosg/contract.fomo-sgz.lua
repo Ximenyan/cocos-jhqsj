90666d3949c4241b4bb4c82dc099be28681220d0
wangwei <347364217@qq.com>
2020-11-09 14:20:08 +0800

    fomo

--- contract/fomosg/contract.fomo-sgz.lua
+++ contract/fomosg/contract.fomo-sgz.lua
@@ -1,9 +1,9 @@
 CONTRACT_TOKEN = "contract.fomo-token"
 CONTRACT_CONFIGS = "contract.fomo-configs"
 DEVELOPER_PROPORTION = 0.02 -- 开发者占比
-BOUNS_PROPORTION = 0.60 -- 分红占比
+BOUNS_PROPORTION = 0.70 -- 分红占比
 FINAL_PRIZE = 0.23 -- 最终大奖
-WIN_COUNTRY = 0.15 -- 获胜国家分红
+Next_Round = 0.05 -- 下一轮
 
 local function _read_data()
     -- 读数据
@@ -29,13 +29,13 @@ function init()
         count = 1, -- 轮数
         total_amount = 0, -- 总流水
         count_amount = 0, -- 本轮总流水
-        win_country_amount = 0, -- 获胜国家分红
+        next_round_pool_amount = 0, -- 流入到下一轮的资金
         final_prize = 0, -- 最终大奖
         timestamp = chainhelper:time() + 8 * 60 * 60,
         countrys = {
-            Wei = {num = 0, price = 0},
-            Shu = {num = 0, price = 0},
-            Wu = {num = 0, price = 0}
+            Donghan = {num = 0, price = 0},
+            -- Shu = {num = 0, price = 0},
+            -- Wu = {num = 0, price = 0}
         }
     }
     _save_data()
@@ -44,6 +44,7 @@ end
 function Buy(arg_country, arg_num)
     _read_data()
     _ContractConfig()
+    arg_country = "Donghan"
     assert(contract_base_info.invoker_contract_id == "1.16.0",
            "#没有权限！#")
     CToken = import_contract(CONTRACT_TOKEN)
@@ -58,32 +59,35 @@ function Buy(arg_country, arg_num)
         chainhelper:log(public_data.winner .. '  Won the war！amount :' ..
                             (winner_amount / 100000))
         -- 转给获胜人
-        chainhelper:transfer_from_caller(public_data.winner, winner_amount,
+        if public_data.winner ~= contract_base_info.caller then
+            chainhelper:transfer_from_caller(public_data.winner, winner_amount,
                                          "COCOS", true)
+        end
         local call_amount = public_data.final_prize - winner_amount -- 这是发给开奖人的
         chainhelper:log(
             contract_base_info.caller .. ' Draw a prize! amount :' ..
                 (call_amount / 100000))
-        local win_country = public_data.countrys[public_data.win_country]
-        chainhelper:log(public_data.win_country .. ' Carve Up Amount :' ..
-                            (public_data.win_country_amount / 100000))
-        win_country.price = win_country.price +
-                                math.floor(
-                                    public_data.win_country_amount /
-                                        win_country.num) -- 分红给战胜国
+        -- local win_country = public_data.countrys[public_data.win_country]
+        -- chainhelper:log(public_data.win_country .. ' Carve Up Amount :' ..
+        --                     (public_data.win_country_amount / 100000))
+        -- win_country.price = win_country.price +
+        --                         math.floor(
+        --                             public_data.next_round_pool_amount /
+        --                                 win_country.num) -- 分红给战胜国
         public_data.count = public_data.count + 1
-        public_data.count_amount = 0
-        public_data.win_country_amount = 0
+        public_data.count_amount = public_data.next_round_pool_amount
+        public_data.next_round_pool_amount = 0
         public_data.final_prize = 0
         public_data.timestamp = chainhelper:time() + 8 * 60 * 60 -- 新一轮战争时间新增加8h
-        chainhelper:log(public_data.win_country ..
+        chainhelper:log(public_data.winner ..
                             " Won the war!A new round of war has begun!")
     end
     if private_data.countrys == nil then
         private_data.countrys = {
-            Wei = {num = 0, price = 0},
-            Shu = {num = 0, price = 0},
-            Wu = {num = 0, price = 0}
+            Donghan = {num = 0, price = 0},
+            --Wei = {num = 0, price = 0},
+            --Shu = {num = 0, price = 0},
+            --Wu = {num = 0, price = 0}
         }
     end
     local public_country = public_data.countrys[arg_country]
@@ -91,15 +95,15 @@ function Buy(arg_country, arg_num)
     local amount = arg_num * 100000000
     local caller_balance = chainhelper:get_account_balance(contract_base_info.caller,"COCOS")
     assert(caller_balance >= amount,"#Insufficient account balance#")
-    local bonus_amount = math.floor(amount * BOUNS_PROPORTION / 3)
+    local bonus_amount = math.floor(amount * BOUNS_PROPORTION)
     local final_prize = math.floor(amount * FINAL_PRIZE)
     local dev_amount = math.floor(amount * DEVELOPER_PROPORTION)
-    local win_country_amount = math.floor(amount * WIN_COUNTRY)
+    local next_round_pool_amount = math.floor(amount * Next_Round)
     chainhelper:log('Pay for development costs' .. (dev_amount / 100000))
-    chainhelper:log('Bonus amount' .. (bonus_amount * 3 / 100000))
+    chainhelper:log('Bonus amount' .. (bonus_amount / 100000))
     chainhelper:log('Into the prize pool' .. (final_prize / 100000))
-    chainhelper:log('Into the winner medal pool' ..
-                        (win_country_amount / 100000))
+    chainhelper:log('Into the Next Round Pool' ..
+                        (next_round_pool_amount / 100000))
     chainhelper:log('Lock Asset:' .. (amount - dev_amount) / 100000)
     CToken.TransferIn("COCOS", amount - dev_amount)
     chainhelper:transfer_from_caller(G_CONFIG.DEVELOPER_ACCOUNT, dev_amount,
@@ -127,16 +131,16 @@ function Buy(arg_country, arg_num)
                                    math.floor(amount / 100000) -- 本轮流水
     public_data.final_prize = public_data.final_prize + final_prize -- 最终大奖
     public_data.timestamp = public_data.timestamp + arg_num * 10 -- 战争时间延长
-    public_data.win_country = arg_country -- 记录领先国家
+    --public_data.win_country = arg_country -- 记录领先国家
     public_data.winner = contract_base_info.caller -- 记录领先玩家
-    public_data.win_country_amount = public_data.win_country_amount +
-                                         win_country_amount
+    public_data.next_round_pool_amount = public_data.next_round_pool_amount + next_round_pool_amount
     _save_data()
 end
 
 function Withdraw(arg_country)
     _read_data()
     _ContractConfig()
+    arg_country = "Donghan"
     assert(contract_base_info.invoker_contract_id == "1.16.0",
            "#没有权限！#")
     CToken = import_contract(CONTRACT_TOKEN)
